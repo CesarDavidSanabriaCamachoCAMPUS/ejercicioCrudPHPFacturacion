6374c6a8b7e545183fc277318b047942b7fba6c6
CesarDavidSanabriaCamachoCAMPUS <cesar.sanabria1307@gmail.com>
2023-06-07 17:31:07 -0500

    feat: :tada: crud php
    crud de un formulario de los campers en php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>formularioCampusLands</title>
+</head>
+<body>
+    <div class="container1">
+        <div class="section1.1">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="lastName" placeholder="Last Name">
+            <input type="text" name="adress" placeholder="Adress">
+        </div>
+        <div class="section1.2">
+            <div class=isotype><img src="" alt=""></div>
+            <input type="number" name="age" placeholder="Age">
+            <input type="email" name="email" placeholder="E-mail">
+        </div>
+    </div>
+    <div class="container2">
+        <div class="section2.1">
+            <input type="text" name="entryTime" placeholder="Entry time">
+            <input type="text" name="team" placeholder="Team">
+            <input type="text" name="trainer" placeholder="Trainer">
+        </div>
+        <div class="section2.2">
+            <div class="item1">
+                <button></button>
+                <button></button>
+            </div>
+            <div class="item2">
+                <button></button>
+                <button></button>
+            </div>
+            <div class="item3">
+                <input type="number" name="id" placeholder="I.D">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
